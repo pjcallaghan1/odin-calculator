032f4ae441359a7ef5d4c83f5bd6f010d763cbd9
Patrick Callaghan <130389389+pjcallaghan1@users.noreply.github.com.>
2024-10-18 13:08:52 -0400

    commit .html, .js, and .css

---
+++ calculator.js
@@ -0,0 +1,37 @@
+let num1 = 0
+let num2 = 0
+let operator = ''
+
+function add(num1, num2) {
+    let addedNum = num1 + num2;
+    return addedNum;
+};
+
+function subtract(num1, num2) {
+    let addedNum = num1 - num2;
+    return addedNum;
+};
+
+function multiply(num1, num2) {
+    let addedNum = num1 * num2;
+    return addedNum;
+};
+
+function divide(num1, num2) {
+    let addedNum = num1 / num2;
+    return addedNum;
+};
+
+function operate(operator, num1, num2) {
+    if (operator === 'add'){
+        add(num1,num2);
+    } else if (operator === 'subtract'){
+        subtract(num1,num2);
+    } else if (operator === 'multipy'){
+        multiply(num1,num2);
+    } else if (operator === 'divide'){
+        divide(num1,num2);
+    } else {
+        alert('Sorry, I couldnt understand that.')
+    }
+};
